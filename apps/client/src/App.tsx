f61a7efc2d9b3045142a8f8a67e35190428b3aaa
hecsolmay <hecsolmay@gmail.com>
2024-06-28 10:13:55 -0600

    change fetch on client page

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,7 +3,7 @@ import { API_URL } from './config'
 export default function App () {
   const handleClick = async () => {
     try {
-      const response = await fetch(`${API_URL}/api`)
+      const response = await fetch(`${API_URL}`)
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
