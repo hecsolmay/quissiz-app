6bd6db62d16dbab66033932041a045863ff4a377
hecsolmay <hecsolmay@gmail.com>
2024-06-29 15:48:53 -0600

    add lobby component and add lobby container on loading

--- apps/client/src/components/loading.tsx
+++ apps/client/src/components/loading.tsx
@@ -1,9 +1,13 @@
+import { LobbyActions } from './actions'
 import LoadingSpinner from './icons/loading-spinner'
 
 export default function Loading () {
   return (
-    <div className='grid min-h-[70dvh] w-full place-items-center'>
-      <LoadingSpinner />
-    </div>
+    <>
+      <LobbyActions />
+      <div className='grid min-h-[70dvh] w-full place-items-center'>
+        <LoadingSpinner />
+      </div>
+    </>
   )
 }
--- apps/client/src/components/lobby.tsx
+++ apps/client/src/components/lobby.tsx
@@ -1,3 +1,40 @@
+import { useTestsStore } from '../store/tests'
+import { LobbyActions } from './actions'
+
 export default function Lobby () {
-  return (<div>Lobby</div>)
+  const tests = useTestsStore(state => state.tests)
+  const setSelectedTest = useTestsStore(state => state.setSelectedTest)
+
+  const createHandleClick = (testId: string) => () => {
+    setSelectedTest(testId)
+  }
+
+  return (
+    <>
+      <LobbyActions />
+      <div>
+        <h1 className='text-center text-4xl font-bold'>
+          Selecciona el examen que quieras jugar
+        </h1>
+        <div className='mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+          {tests.map(test => (
+            <div
+              key={test.id}
+              onClick={createHandleClick(test.id)}
+              className='group relative h-64 cursor-pointer overflow-hidden rounded-lg border border-gray-200 p-4 shadow-slate-900'
+            >
+              <img
+                src={test.imageUrl}
+                alt={`Imagen del examen ${test.name}`}
+                className='absolute inset-0 z-0 size-full rounded-lg object-fill transition-all duration-200 group-hover:scale-105 group-hover:opacity-75'
+              />
+              <div className='absolute bottom-0 left-0 z-10 my-auto flex h-14 w-full items-center rounded-b-lg bg-black/70 px-4 py-2 text-sm text-white'>
+                <h3 className='truncate text-lg font-medium'>{test.name}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
 }
