cc5a72c1593e12318cbe76c30c5bdf6518ef7b1c
hecsolmay <hecsolmay@gmail.com>
2024-06-30 02:12:00 -0600

    reuse db connection if is already open

--- apps/server/src/database/database.ts
+++ apps/server/src/database/database.ts
@@ -1,10 +1,16 @@
 import { MONGODB_URI } from '../config'
 import mongoose from 'mongoose'
 
+let db: typeof mongoose | null = null
+
 const connect = async () => {
   try {
     console.log('Connecting to MongoDB...')
-    await mongoose.connect(MONGODB_URI, {})
+    if (db !== null) {
+      console.log('Already connected to MongoDB')
+      return db
+    }
+    db = await mongoose.connect(MONGODB_URI, {})
     console.log('Connected to MongoDB')
   } catch (err) {
     console.error(err)
