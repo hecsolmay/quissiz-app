c6ef7777e9bd52396c41ba232e14ed59d8f435ed
hecsolmay <hecsolmay@gmail.com>
2024-06-29 20:59:08 -0600

    add new Components on game, gameover and gameresults components

--- apps/client/src/components/game.tsx
+++ apps/client/src/components/game.tsx
@@ -1,6 +1,16 @@
+import { APP_STATUS } from '../constants'
+import useQuestionsData from '../hooks/useQuestionsData'
 import { cn } from '../libs/cn'
 import { useTestsStore } from '../store/tests'
+import { GameActions } from './actions'
+import { Button } from './buttons'
+import { GameContainer } from './containers'
 import Error from './error'
+import { GameFooter } from './game-footer'
+import ChevronArrow from './icons/chevron-arrow'
+import Clock from './icons/clock'
+import Crown from './icons/crown'
+import Questions from './questions'
 
 export function Game () {
   const test = useTestsStore(state => state.selectedTest)
@@ -8,68 +18,158 @@ export function Game () {
   const currentQuestionIndex = useTestsStore(
     state => state.currentQuestionIndex
   )
-  const saveAnswer = useTestsStore(state => state.saveAnswer)
+  const goToNextQuestion = useTestsStore(state => state.goToNextQuestion)
+  const changeAppStatus = useTestsStore(state => state.changeAppStatus)
+  const { unanswered } = useQuestionsData()
 
   if (test === null) {
     return <Error />
   }
 
   const currentQuestion = questions[currentQuestionIndex]
-  const {
-    isUserAlreadyAnswered,
-    isCorrectAnswer,
-    correctIndex,
-    userSelectedOption
-  } = currentQuestion
-
-  const getOptionColor = (index: number) => {
-    if (!isUserAlreadyAnswered)
-      return 'border border-gray-300 bg-white text-gray-800'
-
-    if (userSelectedOption === index && !isCorrectAnswer) {
-      return 'bg-red-500'
-    }
-
-    if (index === correctIndex) {
-      return 'bg-green-500'
-    }
-
-    return 'border border-gray-300 bg-white text-gray-800'
-  }
-
-  const handleAnswer = (index: number) => () => {
-    saveAnswer(currentQuestion.id, index)
-  }
+  const { isUserAlreadyAnswered } = currentQuestion
 
   return (
-    <section className='flex flex-col items-center gap-6'>
-      <h1 className='text-pretty text-3xl font-bold'>{test.name}</h1>
-      <article className='w-full rounded-lg bg-white p-4 shadow-lg sm:p-8 md:w-96'>
-        <h1 className='text-2xl font-semibold text-gray-800'>
-          {currentQuestion.question}
-        </h1>
-        <div className='mt-8 flex flex-col gap-2'>
-          {currentQuestion.options.map((option, index) => (
-            <button
-              key={index}
-              disabled={isUserAlreadyAnswered}
-              className={cn(
-                'w-full rounded-lg px-4 py-2 text-sm shadow-sm hover:opacity-80 transition-colors duration-200',
-                { 'cursor-default hover:opacity-100': isUserAlreadyAnswered },
-                getOptionColor(index),
-                'disabled:opacity-70'
-              )}
-              onClick={handleAnswer(index)}
+    <>
+      <GameActions />
+      <GameContainer>
+        <h1 className='text-pretty text-3xl font-bold'>{test.name}</h1>
+        <Questions>
+          <div className='mt-4 inline-flex min-h-7 w-full justify-end'>
+            {isUserAlreadyAnswered && unanswered === 0 && (
+              <Button
+                onClick={() => changeAppStatus(APP_STATUS.GAME_OVER)}
+                className='border-gray-500 bg-black/80 text-white'
+              >
+                Ver resultados
+              </Button>
+            )}
+
+            {isUserAlreadyAnswered && unanswered > 0 && (
+              <Button
+                className='border-gray-500 bg-black/80 text-white'
+                onClick={goToNextQuestion}
+              >
+                Siguiente Pregunta
+              </Button>
+            )}
+          </div>
+        </Questions>
+
+        <GameFooter />
+      </GameContainer>
+    </>
+  )
+}
+
+export function GameResults () {
+  const currentQuestionIndex = useTestsStore(
+    state => state.currentQuestionIndex
+  )
+  const questions = useTestsStore(state => state.questions)
+  const goToNextQuestion = useTestsStore(state => state.goToNextQuestion)
+  const goToPreviousQuestion = useTestsStore(
+    state => state.goToPreviousQuestion
+  )
+
+  const currentQuestion = questions[currentQuestionIndex]
+  const { timeAnswered } = currentQuestion
+  const initialSeconds = useTestsStore(state => state.initialSeconds)
+
+  const hasPrev = currentQuestionIndex > 0
+
+  return (
+    <>
+      <GameActions />
+      <GameContainer>
+        <header>
+          <h1 className='text-pretty text-3xl font-bold'>
+            Revision de los resultados
+          </h1>
+
+          <p className='mt-2 text-end'>
+            Pregunta {currentQuestionIndex + 1} de {questions.length}
+          </p>
+
+          <div className='inline-flex items-center gap-1'>
+            <Clock />
+            <span>
+              {timeAnswered}s de {initialSeconds}s
+            </span>
+          </div>
+        </header>
+
+        <Questions>
+          <div
+            className={cn(
+              'mt-4 inline-flex min-h-7 w-full justify-between',
+              !hasPrev && 'justify-end'
+            )}
+          >
+            {hasPrev && (
+              <Button
+                onClick={goToPreviousQuestion}
+                className='border-gray-500 bg-black/80 px-2 text-white'
+              >
+                <ChevronArrow direction='left' />
+              </Button>
+            )}
+
+            <Button
+              className='border-gray-500 bg-black/80 px-2 text-white'
+              onClick={goToNextQuestion}
             >
-              {option}
-            </button>
-          ))}
-        </div>
-      </article>
-    </section>
+              <ChevronArrow direction='right' />
+            </Button>
+          </div>
+        </Questions>
+
+        <GameFooter />
+      </GameContainer>
+    </>
   )
 }
 
 export function GameOver () {
-  return <div>Game Over</div>
+  const { message } = useQuestionsData()
+  const changeAppStatus = useTestsStore(state => state.changeAppStatus)
+  const changeCurrentQuestion = useTestsStore(
+    state => state.changeCurrentQuestion
+  )
+  const resetGame = useTestsStore(state => state.resetGame)
+
+  const handleClick = () => {
+    changeCurrentQuestion(0)
+    changeAppStatus(APP_STATUS.GAME_RESULTS)
+  }
+
+  return (
+    <>
+      <GameActions />
+      <div className='grid min-h-[70dvh] place-items-center'>
+        <div className='min-h-72 rounded-lg bg-white p-4 text-black shadow-lg sm:p-8 md:w-96'>
+          <div className='flex w-full justify-center'>
+            <Crown className='size-24 font-bold' />
+          </div>
+
+          <div className='mt-4 text-center'>
+            <h1 className='text-pretty text-xl font-bold'>
+              Haz terminado tu examen
+            </h1>
+            <p className='mt-3 text-slate-600'>{message}</p>
+          </div>
+
+          <div className='mt-4 flex w-full justify-center gap-4'>
+            <Button onClick={resetGame}>Reiniciar</Button>
+            <Button
+              onClick={handleClick}
+              className='border-gray-500 bg-black/80 px-2 text-white'
+            >
+              Ver resultados
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
