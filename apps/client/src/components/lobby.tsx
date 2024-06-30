9618e347857885426af7e12d3462a7be189f3548
hecsolmay <hecsolmay@gmail.com>
2024-06-30 02:12:52 -0600

    fix accessibility errors

--- apps/client/src/components/lobby.tsx
+++ apps/client/src/components/lobby.tsx
@@ -26,10 +26,10 @@ export default function Lobby () {
               <img
                 src={test.imageUrl}
                 alt={`Imagen del examen ${test.name}`}
-                className='absolute inset-0 z-0 size-full rounded-lg object-fill transition-all duration-200 group-hover:scale-105 group-hover:opacity-75'
+                className='absolute inset-0 z-0 aspect-video size-full rounded-lg object-fill transition-all duration-200 group-hover:scale-105 group-hover:opacity-75'
               />
               <div className='absolute bottom-0 left-0 z-10 my-auto flex h-14 w-full items-center rounded-b-lg bg-black/70 px-4 py-2 text-sm text-white'>
-                <h3 className='truncate text-lg font-medium'>{test.name}</h3>
+                <h2 className='truncate text-lg font-medium'>{test.name}</h2>
               </div>
             </div>
           ))}
